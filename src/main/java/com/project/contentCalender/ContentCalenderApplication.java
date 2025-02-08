79d05f35773ad8135921e612bdf3cc94a5526674
Mukul Howale <howalemukul@gmail.com>
2025-02-08 20:33:40 +0530

    Update

--- src/main/java/com/project/contentCalender/ContentCalenderApplication.java
+++ src/main/java/com/project/contentCalender/ContentCalenderApplication.java
@@ -2,12 +2,13 @@ package com.project.contentCalender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ContentCalenderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ContentCalenderApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(ContentCalenderApplication.class, args);
 	}
 
 }
