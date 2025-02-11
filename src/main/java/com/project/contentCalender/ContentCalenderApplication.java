6b3d4b71e8a8665bafe90c4abc0a03758ac314a0
Mukul Howale <howalemukul@gmail.com>
2025-02-11 15:51:20 +0530

    Way to get all beans

--- src/main/java/com/project/contentCalender/ContentCalenderApplication.java
+++ src/main/java/com/project/contentCalender/ContentCalenderApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import javax.naming.Context;
+import java.util.Arrays;
+
 @SpringBootApplication
 public class ContentCalenderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ContentCalenderApplication.class, args);
+		ConfigurableApplicationContext context =  SpringApplication.run(ContentCalenderApplication.class, args);
+		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 	}
 
 }
